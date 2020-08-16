b811a4374389ab9d1806765592d99a4bbdbc9793
Tomas Perez <tomasitopg6@gmail.com>
2020-08-16 07:56:29 +0000

    finished up laying out video game controller, finished user erb files

--- app/controllers/favorite_video_games_controller.rb
+++ app/controllers/favorite_video_games_controller.rb
@@ -12,4 +12,34 @@ class FavoriteVideoGamesController < ApplicationController
     erb :'/favorite_video_games/index'
   end
 
+  get '/favoritevideogames/new' do
+    erb :'/favorite_video_games/new'
+  end
+
+  get '/favoritevideogames/:id' do
+    @favoritevideogame = FavoriteVideoGames.find(params[:id])
+
+    erb :'/favorite_video_games/show'
+  end
+
+  get '/favorite_video_games/:id/edit' do
+    @favoritevideogame = FavoriteVideoGames.find(params[:id])
+
+    erb :'/favorite_video_games/edit'
+  end
+
+  patch '/favorite_video_games/:id' do
+    @favoritevideogame = FavoriteVideoGames.find(params[:id])
+    @favoritevideogame.update(name: params[:name])
+
+    erb :'/favorite_video_games/show'
+  end
+
+  delete '/favorite_video_games/:id/delete' do
+    @favoritevideogame = FavoriteVideoGames.find(params[:id])
+    @favoritevideogame.delete
+
+    erb :'/favorite_video_games/delete'
+  end
+
 end
