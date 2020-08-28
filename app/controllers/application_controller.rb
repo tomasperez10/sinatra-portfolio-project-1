40a66c7f9058cec91538c3dbaee21067a7a3ce2a
Tomas Perez <tomasitopg6@gmail.com>
2020-08-28 07:06:48 +0000

    changes to models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < Sinatra::Base
     end
 
     def current_user
-      User.find(session[:user_id])
+      User.find(session[:user_id]) if session[:user_id]
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
   has_many :favorite_video_games
 
   has_secure_password
+
+  def self.authenticate(params)
+   user = User.find_by_name(params[:username])
+   (user && user.password == params[:password]) ? user : nil
+  end
 end
