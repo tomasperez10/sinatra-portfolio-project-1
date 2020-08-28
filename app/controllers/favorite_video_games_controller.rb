955d948169ccc36d2b31e79d271233bde0e2aae8
Tomas Perez <tomasitopg6@gmail.com>
2020-08-28 06:32:02 +0000

    associating objects

--- app/controllers/favorite_video_games_controller.rb
+++ app/controllers/favorite_video_games_controller.rb
@@ -2,17 +2,29 @@ class FavoriteVideoGamesController < ApplicationController
 
   get '/favoritevideogames' do
     @favoritevideogames = FavoriteVideoGames.all
+    @users = User.all
     erb :'/favorite_video_games/index'
   end
 
   post '/favoritevideogames' do
-    FavoriteVideoGames.create(name: params[:name], user_id: session[:user_id])
+    #@user = user
+    @favoritevideogame = FavoriteVideoGames.create(name: params[:name], user_id: session[:user_id])
     @favoritevideogames = FavoriteVideoGames.all
+
+    @users = User.all
+    #@user = User.find(:id => params[:id])
+
+    # if !params["user_id"]["name"].empty?
+    #   @favoritevideogame.user = User.create(name: params["user_id"]["name"])
+    # end
+    # @favoritevideogame.save
+    # redirect to "/favoritevideogames/#{@favoritevideogame.id}"
 
     erb :'/favorite_video_games/index'
   end
 
   get '/favoritevideogames/new' do
+    @users = User.all
     erb :'/favorite_video_games/new'
   end
 
@@ -30,13 +42,24 @@ class FavoriteVideoGamesController < ApplicationController
 
   patch '/favoritevideogames/:id' do
     @favoritevideogame = FavoriteVideoGames.find(params[:id])
+    #@user = User.find(:id => params[:id])
     @favoritevideogame.update(name: params[:name])
+
+    unless current_user == @favoritevideogame.user
+      redirect to "/favoritevideogames" and return
+    end
+
+    # if !params["user_id"]["name"].empty?
+    #   @favoritevideogame.user = User.create(name: params["user_id"]["name"])
+    # end
+    # @favoritevideogame.save
 
     redirect to "/favoritevideogames/#{@favoritevideogame.id}"
   end
 
   delete '/favoritevideogames/:id/delete' do
     @favoritevideogame = FavoriteVideoGames.find(params[:id])
+    #@user = User.find(:id => params[:id])
     @favoritevideogame.delete
 
     redirect to '/favoritevideogames'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,14 @@ class UsersController < ApplicationController
   end
 
   get '/signup' do
+    @users = User.all
     erb :'/users/signup'
   end
 
   post '/signup' do
-    user = User.new(:username => params[:username], :password => params[:password])
+    @user = User.new(:id => params[:id], :username => params[:username], :password => params[:password])
 
-    if user.save
+    if @user.save
       redirect "/login"
     else
       redirect "/failure"
@@ -19,14 +20,15 @@ class UsersController < ApplicationController
   end
 
   get '/login' do
+    @users = User.all
     erb :'/users/login'
   end
 
   post '/login' do
-    user = User.find_by(:username => params[:username])
+    @user = User.find_by(:username => params[:username])
 
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect "/favoritevideogames"
     else
       redirect "/failure"
